759061115c9a3391c24f446029dec1643793b72d
riyazriya010 <riyur017@gmail.com.com>
2024-11-16 13:04:24 +0530

    .env aded

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const redis = require('redis');
 const axios = require('axios');
 
 const app = express();
+const PORT = process.env.APP_PORT || 3001
+
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 const DEFAULT_EXPIRATION = 3600;
+
 const redisClient = redis.createClient({
-    host: '127.0.0.1',  // Ensure this is the correct host
-    port: 6379,         // Ensure this is the correct port
+    host: process.env.HOST,
+    port: process.env.PORT,
 });
 
 redisClient.connect()
@@ -45,17 +49,20 @@ app.get('/photos', async (req, res) => {
     const albumId = req.query.albumId;
 
     try {
-        // Wait for Redis to be connected before making requests
+
         if (redisClient.isOpen) {
-            // Fetch all photos from API
             const { data } = await axios.get('https://jsonplaceholder.typicode.com/photos', {
                 params: { albumId },
             });
 
             // Iterate through each photo and store it in Redis with its unique id
-            data.forEach(async (photo) => {
-                await redisClient.setEx(`photos:${albumId}`, DEFAULT_EXPIRATION, JSON.stringify(photo));
-            });
+            // data.forEach(async (photo) => {
+            //     redisClient.setEx(`photos:${albumId}`, DEFAULT_EXPIRATION, JSON.stringify(photo));
+            // });
+
+            for (const photo of data) {
+                redisClient.setEx(`photos:${photo.id}`, DEFAULT_EXPIRATION, JSON.stringify(photo));
+            }
 
             res.json({ datas: data });
         } else {
@@ -69,33 +76,34 @@ app.get('/photos', async (req, res) => {
 });
 
 app.get('/photo', async (req, res) => {
-    const id  = req.query.id;
+    const id = req.query.id;
 
     try {
-        // Wait for Redis to be connected before making requests
+
         if (redisClient.isOpen) {
             console.log('Redis client is open');
-            // Check Redis cache for the specific photo
-            await redisClient.get(`photos:${id}`, async (err, photo) => {
+
+            redisClient.get(`photos:${id}`, async (err, photo) => {
+                console.log('get')
                 if (err) {
                     console.error('Error while getting from Redis', err);
                     return res.status(500).json({ error: 'Error fetching data from Redis' });
                 }
                 console.log('Redis GET result:', photo);
-                // If photo exists in cache, return it
-                if (photo !== null) {
+
+                if (photo != null) {
                     console.log('Cache Hit');
-                    return res.json(JSON.parse(photo));  // Ensure we are returning the JSON response here
+                    return res.json(JSON.parse(photo));
                 }
 
-                // If photo is not in cache, fetch from the API
+
                 console.log('Cache Miss');
                 try {
                     const { data } = await axios.get(`https://jsonplaceholder.typicode.com/photos/${id}`);
 
-                    // Store the specific photo in Redis
-                    await redisClient.setEx(`photos:${id}`, DEFAULT_EXPIRATION, JSON.stringify(data));
-                    return res.json(data);  // Send response once the data is fetched and cached
+
+                    redisClient.setEx(`photos:${id}`, DEFAULT_EXPIRATION, JSON.stringify(data));
+                    return res.json(data);
                 } catch (err) {
                     console.error('Error fetching photo from API:', err);
                     return res.status(500).json({ error: 'Error fetching photo from API' });
@@ -111,6 +119,6 @@ app.get('/photo', async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
